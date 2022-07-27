d76b280654c128588f42758d4a7a61f1769eb447
jhengyulee <chi1987615@hotmail.com>
2022-07-27 16:27:31 +0800

    新增作品集頁面

---
+++ base.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set('Asia/Taipei');
+session_start();
+
+function pdo(){
+
+    $dsn="mysql:host=localhost;charset=utf8;dbname=resume";
+    $pdo=new PDO($dsn,'root','',[]);
+    $sql="SELECT * FROM `` ";
+}
+?>
+
+
+
+
+
+
+    
