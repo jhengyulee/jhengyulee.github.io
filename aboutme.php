84fbc3c4a6c3505cbc761bce0423123cae0a1242
unknown <chi1987615@hotmail.com>
2022-08-07 23:00:02 +0800

    新增about me頁面

---
+++ aboutme.php
@@ -0,0 +1,112 @@
+
+    <div class="container">
+
+        <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>About Me</h2>
+                    <p>畢業於國立新竹教育大學，主修教育心理與諮商。
+
+                        任教於國中時，擔任校園科學營隊工作人員，看著學生們令人驚豔的作品，激起我對網頁設計的好奇及探究心，
+                        所以進入泰山職訓網頁設計專班，歷經半年的學習，漸漸著迷於這個由程式碼、圖文設計及邏輯組成的世界。
+
+                        <br>
+                        目前為止仍在網頁設計領域持續努力學習中！
+                    </p>
+                </div>
+                <!-- 1 -->
+                <div class="row justify-content-around col-12">
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                        <img src="./img/me.jpeg" class="img-fluid rounded-lg">
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                    <h3>1987年  生於港都高雄</h3>
+                    </div>
+                    <div class="col-lg-1"></div>
+                </div>
+                <br>
+                <br>
+                
+                <!-- 2 -->
+                <div class="row justify-content-around col-12">
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                        
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem suscipit
+                            iste harum modi accusamus praesentium dolore ipsam magni laboriosam laudantium in dolores
+                            expedita, aperiam  ducimus culpa provident nisi veritatis!
+                            Fuga consectetur magnam totam quas quos! Voluptatem iure repellat eligendi molestiae!
+                            Officia aliquam enim accusamus odit!
+                        </p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                  
+                    <div class="col-lg-4">
+                        <img src="./img/me.jpeg" class="img-fluid rounded-lg" alt="">
+                    </div>
+                    <div class="col-lg-1"></div>
+                </div>
+                <br>
+                <br>
+                
+                <!-- 3 -->
+                <div class="row justify-content-around col-12">
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                        <img src="./img/me.jpeg" class="img-fluid rounded-lg" alt="">
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem suscipit
+                            iste harum modi accusamus praesentium dolore ipsam magni laboriosam laudantium in dolores
+                            expedita, aperiam  ducimus culpa provident nisi veritatis!
+                            Fuga consectetur magnam totam quas quos! Voluptatem iure repellat eligendi molestiae!
+                            Officia aliquam enim accusamus odit!
+                        </p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                </div>
+                <br>
+                <br>
+                
+                <!-- 4 -->
+                <div class="row justify-content-around col-12">
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-4">
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem suscipit
+                            iste harum modi accusamus praesentium dolore ipsam magni laboriosam laudantium in dolores
+                            expedita, aperiam  ducimus culpa provident nisi veritatis!
+                            Fuga consectetur magnam totam quas quos! Voluptatem iure repellat eligendi molestiae!
+                            Officia aliquam enim accusamus odit!
+                        </p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                  
+                    <div class="col-lg-4">
+                        <img src="./img/me.jpeg" class="img-fluid rounded-lg" alt="">
+                    </div>
+                    <div class="col-lg-1"></div>
+                </div>
+
+            </div>
+        </section>
+
+  
+
+
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+            integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
+        </script>
+
---
+++ portfolio.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>作品集</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/digi.css">
+    
+
+</head>
+
+<body>
+    <div class="container">
+        <ul class="nav nav-tabs bg-light" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-toggle="tab" data-target="#home" type="button"
+                    role="tab" aria-controls="home" aria-selected="true">圖文設計</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-toggle="tab" data-target="#profile" type="button"
+                    role="tab" aria-controls="profile" aria-selected="false">Back-End</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-toggle="tab" data-target="#contact" type="button"
+                    role="tab" aria-controls="contact" aria-selected="false">Front-End</button>
+            </li>
+           
+        </ul>
+
+
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <div class="container mt-4">
+                   
+                    <div class="row justify-content-between">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/bird.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">30-days animals challenge</h5>
+                                <p class="card-text">
+                                    <!-- icon -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" fill="currentColor"
+                                        class="bi bi-sun" viewBox="0 0 24 24">
+                                        <path
+                                            d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                                    </svg>
+                                    <!-- icon end -->
+                                    <br>
+                                    利用黃金比例幾何圖形，描繪出動物外型輪廓，以漸層方式將其上色
+                                </p>
+                                <a href="#" class="btn btn-primary">點我看看</a>
+                            </div>
+                        </div>
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/calendar.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">兔年大吉電子年曆</h5>
+                                <p class="card-text">
+                                    <!-- icon -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" fill="currentColor"
+                                        class="bi bi-sun" viewBox="0 0 24 24">
+                                        <path
+                                            d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                                    </svg>
+                                    <!-- icon end -->
+                                    <br>
+                                    以紅色為基礎色，結合拓印兔子輪廓，期望新的一年能嶄新開始
+                                </p>
+                                <a href="#" class="btn btn-primary">點我看看</a>
+                            </div>
+                        </div>
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/poster.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">旅遊電子報</h5>
+                                <p class="card-text">
+                                    <!-- icon -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" fill="currentColor"
+                                        class="bi bi-sun" viewBox="0 0 24 24">
+                                        <path
+                                            d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                                    </svg>
+                                    <!-- icon end -->
+                                    <br>
+                                    陽光、沙灘、海洋，三種元素編織後疫情時代旅遊的想像
+                                </p>
+                                <a href="#" class="btn btn-primary">點我看看</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="container mt-4">
+                    <div class="row justify-content-between">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/banner.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">網頁橫幅設計</h5>
+                                <p class="card-text">
+                                    <!-- icon -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" fill="currentColor"
+                                        class="bi bi-sun" viewBox="0 0 24 24">
+                                        <path
+                                            d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                                    </svg>
+                                    <!-- icon end -->
+                                    <br>
+                                    初來乍到的第一份作品。用幾何圖形拼湊，呈現職業訓練簡單有力、實事求是的風格</p>
+                                <a href="#" class="btn btn-primary">點我看看</a>
+                            </div>
+                        </div>
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/imagine.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">To be continued...</h5>
+                                <p class="card-text">敬請期待</p>
+                                <!-- <a href="#" class="btn btn-primary">點我看看</a> -->
+                            </div>
+                        </div>
+                        <div class="card" style="width: 18rem;">
+                            <img src="./img/imagine.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">To be continued...</h5>
+                                <p class="card-text">敬請期待</p>
+                                <!-- <a href="#" class="btn btn-primary">點我看看</a> -->
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">222</div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">333</div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">444</div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+        integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
